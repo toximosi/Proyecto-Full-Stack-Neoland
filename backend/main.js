871c7b89b8ec718e9717b5e71587e7380f694fff
toximosi <toximosi@gmail.com>
2020-03-30 20:19:42 +0200

    En Backend: arrancando el servidor base. En Frontend: modificaciones de scss (pruebas)

--- backend/main.js
+++ backend/main.js
@@ -1,20 +1,32 @@
-//! Centro neural del servidor -----------------------
+//! 🤘 Centro neural del servidor -----------------------
+// express --> para crear el servidro
 const express = require('express');
+// helmet --> para ayudarnos con la seguridad del servidor
 const helmet = require('helmet');
 
-
-//* montamos el servidor -----------------------------
+//* 👌 montamos el servidor, Oh yeaH!!  -----------------------------
 const server = express();
+
+//* MIDDLEWARE al ataque!!!! 
+//Servidor protegido :)
 server.use(helmet());
 
-// xxxxxxxx -------------------------------------------
+// 👇 AQUÍ EMPIEZA LA API -------------------------------------------
 
-//-----------------------------------------------------
+//! ENDPOINTS ---------------------------------------------------------
+server.get('/', (req, res) => {
+    res.send("<h1> Esto arranca,<br> Metamosle Caña 🤘 !!!!! </h1>")
+})
+//CRUD --------------------------------------------------------------
+// LEER (READ) (GET)
 
-// Endpoint -------------------------------------------
+// CREAR (CREATE) (POST)
 
-//-----------------------------------------------------
+// EDITAR (EDIT) (PUT)
 
+// ELIMINAR (DELETE) (DELETE)
+
+//! LISTEN --------------------------------------------------------------
 
 // --> llamada al servidor en el puerto 3000
 //const PORT = process.env.PORT;
